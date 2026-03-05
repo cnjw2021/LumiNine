97056cb3f7fcfde39b96f08be4eeb42003a8f7a8
aussie9804 <aussie9804@yahoo.co.jp>
2026-03-05 10:01:46 +0900

    SimpleResult 관련 코드 삭제

--- frontend/src/components/features/results/index.ts
+++ frontend/src/components/features/results/index.ts
@@ -1,5 +1,4 @@
 export { default as Result } from './Result';
-export { default as SimpleResult } from './SimpleResult';
 export { default as MainStarKyuseiBoard } from './MainStarKyuseiBoard';
 export { default as ResultFortuneSection } from './ResultFortuneSection';
 export { default as StarAttributesDisplay } from './StarAttributesDisplay';
